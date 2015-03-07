ff2c61be41fa5c777a600b3febaceb126a24a191
Desmond <mynameisdesmond@gmail.com>
2015-03-07 17:39:53 -0500

    link user to tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,7 @@ class TweetsController < ApplicationController
 
 	def create
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
 		if @tweet.save
 			flash[:danger] = "You have created a tweet"
 			redirect_to new_tweet_path		
